4cddc780a8a1ca53bd79baa0e6a5ae05c81f3923
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-04-25 16:41:19 +0300

    Added Navbar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-import ModeToggle from '@/components/ModeToggle';
-import { Button } from '@/components/ui/button';
-import {
-    SignedIn,
-    SignedOut,
-    SignInButton,
-    SignUpButton,
-    UserButton,
-} from '@clerk/nextjs';
-
 export default function Home() {
     return (
         <div className="m-4">
-            <SignedOut>
-                <SignInButton mode="modal">
-                    <Button>Sign In</Button>
-                </SignInButton>
-                <SignUpButton mode="modal" />
-            </SignedOut>
-
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-
-            <ModeToggle />
+            <h1>Home</h1>
         </div>
     );
 }
